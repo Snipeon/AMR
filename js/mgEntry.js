24f5be23f0e9563a2bee0e847dccb6294dc94eb0
Tony Luo <tonywluo@gmail.com>
2017-09-29 18:09:17 -0700

    Adding MangaFox fix

--- js/mgEntry.js
+++ js/mgEntry.js
@@ -25,7 +25,7 @@ var github_repository      = 'https://rawgit.com/twluo/mirrors/master/';
 //To enable local loading, you must add "'unsafe-eval'" to "content_security_policy" in the manifest.json. This is not enabled by default due to security reasons.
 //How to use: Put mirror file in js/mirrors/, add filename (without ext) to localMirrors. Reload extension.
 var localMirrors = [];
-var githubMirrors = ["MangaHere"];
+var githubMirrors = ["MangaHere", "MangaFox"];
 
 //##############################################################################
 // Load websites description and code in one array. Do first load if necessary.
